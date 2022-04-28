6a0350cb074fe05818b10fc4e86946d947d1bf72
ruibinx <xingrobin@gmail.com>
2022-04-28 16:03:39 +0800

    add: tests

--- contracts/TestingERC721Contract.sol
+++ contracts/TestingERC721Contract.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 // only used fro testing
 contract TestingERC721Contract is ERC721Enumerable {
@@ -12,5 +13,9 @@ contract TestingERC721Contract is ERC721Enumerable {
     function mint() public {
 
         _safeMint(msg.sender, totalSupply() + 1);
+    }
+
+    function tokenURI(uint256 tokenId) public pure override returns(string memory) {
+        return string(bytes.concat(bytes("baseUrl"), bytes(Strings.toString(tokenId))));
     }
 }
