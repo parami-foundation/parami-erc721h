89bab98b926f3178ec2c813b6aff6cda648628ef
kaikang.eth <cctv2206@outlook.com>
2023-01-06 15:32:56 +0800

    MOD: deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,14 +8,30 @@ import { ethers,upgrades } from "hardhat";
 async function main() {
   const erc721HBatchCollection = await ethers.getContractFactory("ERC721HBatchCollection");
   const instance = await upgrades.deployProxy(erc721HBatchCollection, ["https://airdrop.parami.io/hnft/", "https://app.parami.io/hnft/ethereum/", 100000]);
-  await instance.deployed()
+  await instance.deployed();
 
-  console.log("proxy deployed to", instance.address)
+  console.log("proxy deployed to", instance.address);
+}
+
+async function deployParamiRegistry() {
+  const paramiRegistryFactory = await ethers.getContractFactory("ParamiRegistry");
+  console.log('Deploying ParamiRegistry...');
+  const instance = await upgrades.deployProxy(paramiRegistryFactory, ["0xD5a6a05cCf0F027c291A4B66B6b3B0086b1A22a7"]);
+  await instance.deployed();
+
+  console.log("parami registry proxy deployed to:", instance.address);
+}
+
+async function deployMockAD3() {
+  const mockAD3Factory = await ethers.getContractFactory("MockAD3");
+  const mockAD3 = await mockAD3Factory.deploy();
+  await mockAD3.deployed();
+  console.log('Mock AD3 deployed to:', mockAD3.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+deployParamiRegistry().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
