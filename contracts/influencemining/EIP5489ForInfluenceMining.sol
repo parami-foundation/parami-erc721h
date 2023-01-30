bdcc55bd61d9e85a167aa1c800d05b68d7c23828
ironman_ch <coderfengyun@gmail.com>
2023-01-30 19:49:55 +0800

    ADD: tests for mint with level

--- contracts/influencemining/EIP5489ForInfluenceMining.sol
+++ contracts/influencemining/EIP5489ForInfluenceMining.sol
@@ -9,7 +9,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../IERC721H.sol";
 import "../base64.sol";
 
-contract EIP5489ForInfluenceMining is IERC721H, ERC721EnumerableUpgradeable, OwnableUpgradeable {
+contract EIP5489ForInfluenceMining is
+    IERC721H,
+    ERC721EnumerableUpgradeable,
+    OwnableUpgradeable
+{
     mapping(uint256 => address) public tokenId2AuthorizedAddress;
     mapping(uint256 => string) public tokenId2ImageUri;
     mapping(uint256 => string) public tokenId2Hyperlink;
@@ -19,61 +23,93 @@ contract EIP5489ForInfluenceMining is IERC721H, ERC721EnumerableUpgradeable, Own
     string private defaultHyperlinkPrefix;
     IERC20 ad3Contract;
 
-    function initialize(address _ad3Address) initializer public {
+    function initialize(address _ad3Address) public initializer {
         __ERC721_init("Hyperlink NFT Collection", "HNFT");
         __Ownable_init();
         ad3Contract = IERC20(_ad3Address);
-     }
+    }
 
     modifier onlyTokenOwner(uint256 tokenId) {
-        require(tx.origin == ownerOf(tokenId) || _msgSender() == ownerOf(tokenId), "should be the token owner");
+        require(
+            tx.origin == ownerOf(tokenId) || _msgSender() == ownerOf(tokenId),
+            "should be the token owner"
+        );
         _;
     }
 
     modifier onlySlotManager(uint256 tokenId) {
-        require(_msgSender() == ownerOf(tokenId) || tokenId2AuthorizedAddress[tokenId] == _msgSender(), "address should be authorized");
+        require(
+            _msgSender() == ownerOf(tokenId) ||
+                tokenId2AuthorizedAddress[tokenId] == _msgSender(),
+            "address should be authorized"
+        );
         _;
     }
 
-    function setSlotUri(uint256 tokenId, string calldata value) override external onlyTokenOwner(tokenId) {
+    function setSlotUri(uint256 tokenId, string calldata value)
+        external
+        override
+        onlyTokenOwner(tokenId)
+    {
         tokenId2Hyperlink[tokenId] = value;
 
         emit SlotUriUpdated(tokenId, _msgSender(), value);
     }
 
-    function getSlotUri(uint256 tokenId, address slotManagerAddr) override external view returns (string memory) {
+    function getSlotUri(uint256 tokenId, address slotManagerAddr)
+        external
+        view
+        override
+        returns (string memory)
+    {
         return tokenId2Hyperlink[tokenId];
     }
 
-    function authorizeSlotTo(uint256 tokenId, address slotManagerAddr) override external onlyTokenOwner(tokenId) {
+    function authorizeSlotTo(uint256 tokenId, address slotManagerAddr)
+        external
+        override
+        onlyTokenOwner(tokenId)
+    {
         if (tokenId2AuthorizedAddress[tokenId] != slotManagerAddr) {
             tokenId2AuthorizedAddress[tokenId] = slotManagerAddr;
             emit SlotAuthorizationCreated(tokenId, slotManagerAddr);
         }
     }
 
-    function revokeAuthorization(uint256 tokenId, address slotManagerAddr) override public onlyTokenOwner(tokenId) {
+    function revokeAuthorization(uint256 tokenId, address slotManagerAddr)
+        public
+        override
+        onlyTokenOwner(tokenId)
+    {
         address authorizedAddress = tokenId2AuthorizedAddress[tokenId];
-        
+
         delete tokenId2Hyperlink[tokenId];
 
         emit SlotAuthorizationRevoked(tokenId, authorizedAddress);
     }
 
-    function revokeAllAuthorizations(uint256 tokenId) override external onlyTokenOwner(tokenId) {
+    function revokeAllAuthorizations(uint256 tokenId)
+        external
+        override
+        onlyTokenOwner(tokenId)
+    {
         revokeAuthorization(tokenId, address(0));
     }
 
-    function isSlotManager(uint256 tokenId, address addr) public view returns (bool) {
+    function isSlotManager(uint256 tokenId, address addr)
+        public
+        view
+        returns (bool)
+    {
         return tokenId2AuthorizedAddress[tokenId] == addr;
     }
 
     function mint(string calldata imageUri, uint256 targetLevel) external {
         uint256 tokenId = totalSupply() + 1;
         _safeMint(msg.sender, tokenId);
-        tokenId2ImageUri[tokenId] = imageUri;  
-        
-        if(targetLevel != 0) {
+        tokenId2ImageUri[tokenId] = imageUri;
+
+        if (targetLevel != 0) {
             _upgradeTo(tokenId, targetLevel);
         }
     }
@@ -85,10 +121,13 @@ contract EIP5489ForInfluenceMining is IERC721H, ERC721EnumerableUpgradeable, Own
         uint256 fromLevelPrice = level2Price[fromLevel];
         uint256 targetLevelPrice = level2Price[targetLevel];
         require(targetLevelPrice != 0, "targetLevel should exist");
-        require(targetLevelPrice > fromLevelPrice, "targetLevelPrice should G.T. fromLevelPrice");
+        require(
+            targetLevelPrice > fromLevelPrice,
+            "targetLevelPrice should G.T. fromLevelPrice"
+        );
 
         uint256 balance = ad3Contract.balanceOf(msg.sender);
-        uint256 priceDiff = targetLevelPrice - fromLevelPrice;        
+        uint256 priceDiff = targetLevelPrice - fromLevelPrice;
         require(balance > priceDiff, "should have enough ad3");
 
         ad3Contract.transferFrom(msg.sender, address(this), priceDiff);
@@ -100,23 +139,31 @@ contract EIP5489ForInfluenceMining is IERC721H, ERC721EnumerableUpgradeable, Own
         _upgradeTo(tokenId, targetLevel);
     }
 
-    function manageLevelPrices(uint256[] calldata levels, uint256[] calldata prices) public onlyOwner() {
-        require(levels.length == prices.length, "levels.size should eq to prices.size");
-        for(uint256 i = 0; i < levels.length; i++) {
+    function manageLevelPrices(
+        uint256[] calldata levels,
+        uint256[] calldata prices
+    ) public onlyOwner {
+        require(
+            levels.length == prices.length,
+            "levels.size should eq to prices.size"
+        );
+        for (uint256 i = 0; i < levels.length; i++) {
             level2Price[levels[i]] = prices[i];
         }
     }
 
-    function withdrawAllAd3() public onlyOwner() {
+    function withdrawAllAd3() public onlyOwner {
         uint256 allBalance = ad3Contract.balanceOf(address(this));
         ad3Contract.transfer(owner(), allBalance);
     }
 
-    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-        require(
-            _exists(_tokenId),
-            "URI query for nonexistent token"
-        );
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(_exists(_tokenId), "URI query for nonexistent token");
 
         return
             string(
@@ -132,11 +179,11 @@ contract EIP5489ForInfluenceMining is IERC721H, ERC721EnumerableUpgradeable, Own
                                 ),
                                 '",',
                                 '"description":"Hyperlink NFT collection created with Parami Foundation"',
-                                ',',
+                                ",",
                                 '"image":"',
                                 tokenId2ImageUri[_tokenId],
                                 '"',
-                                '}'
+                                "}"
                             )
                         )
                     )
@@ -150,6 +197,8 @@ contract EIP5489ForInfluenceMining is IERC721H, ERC721EnumerableUpgradeable, Own
         override(ERC721EnumerableUpgradeable)
         returns (bool)
     {
-        return interfaceId == type(IERC721H).interfaceId || super.supportsInterface(interfaceId);
+        return
+            interfaceId == type(IERC721H).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 }
