b65d7ee11970d7c6707d7e2008b4a0b8cc43b10d
ironman_ch <coderfengyun@gmail.com>
2023-01-31 11:50:48 +0800

    MOD: deploy ad3 with new args

---
+++ scripts/verify/ad3_constructor.ts
@@ -0,0 +1,10 @@
+const adminAddress = "0x9F883b12fD0692714C2f28be6C40d3aFdb9081D3";
+module.exports = [
+  1n * 10n ** 8n * 10n ** 18n,
+  "AD3",
+  "AD3",
+  adminAddress,
+  adminAddress,
+  0,
+  adminAddress,
+];
