1f7fa5a72c43f10b3b43422ec9d628908c31cf64
Ruibin Xing <xingruibin@kanyun.com>
2023-03-27 19:13:05 +0800

    add: support multi asset and chain & impl withdraw

---
+++ contracts/ParamiCCTP.sol
@@ -0,0 +1,98 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract ParamiCCTP is Ownable {
+
+    uint256 public chainDomain;
+
+    mapping (uint256 => address) public assetIdToERC20Contract;
+
+    uint256 nonce;
+
+    mapping (uint256 => mapping ( uint256 => bool)) public sourceDomainToNonceToUsed;
+
+    constructor(uint256 _chainDomain) {
+        chainDomain = _chainDomain;
+    }
+
+    event Deposited(uint256 nonce, uint256 assetId, uint256 amount, uint256 sourceDomain, address sender, uint256 destDomain, bytes destinationRecepient);
+
+    event Withdrawed(uint256 nonce, uint256 assetId, uint256 amount, uint256 sourceDomain, bytes sender, uint256 destDomain, address destinationRecepient);
+
+
+    function registerAsset(uint256 assetId, address erc20Contract) public onlyOwner {
+        assetIdToERC20Contract[assetId] = erc20Contract;
+    }
+
+    function deposit(uint256 assetId, uint256 amount, uint256 destinationDomain, bytes memory destinationRecepient) public {
+        address erc20Contract = assetIdToERC20Contract[assetId];
+        require(erc20Contract != address(0), "asset not registered");
+        ERC20 erc20 = ERC20(erc20Contract);
+        require(erc20.transferFrom(_msgSender(), address(this), amount), "transfer failed");
+
+        nonce++;
+        emit Deposited(nonce, assetId, amount, chainDomain, _msgSender(), destinationDomain, destinationRecepient);
+    }
+
+    function withdraw(uint256 nonce, uint256 assetId, uint256 amount, uint256 sourceDomain, bytes memory sourceSender, address destinationRecepient, bytes memory signature) public onlyOwner {
+        address signer = verifySignature(nonce, assetId, amount, sourceDomain, sourceSender, destinationRecepient, signature);
+        require(sourceDomainToNonceToUsed[sourceDomain][nonce] == false, "nonce already used");
+        require(signer == this.owner(), "invalid signature");
+
+        address erc20Contract = assetIdToERC20Contract[assetId];
+        require(erc20Contract != address(0), "asset not registered");
+
+        sourceDomainToNonceToUsed[sourceDomain][nonce] = true;
+
+        ERC20 erc20 = ERC20(erc20Contract);
+        erc20.transfer(destinationRecepient, amount);
+
+        emit Withdrawed(nonce, assetId, amount, sourceDomain, sourceSender, chainDomain, destinationRecepient);
+    }
+
+    function verifySignature(uint256 nonce, uint256 assetId, uint256 amount, uint256 sourceDomain, bytes memory sourceSender, address destinationRecepient, bytes memory signature) public view returns (address) {
+        bytes32 messageHash = keccak256(abi.encodePacked(nonce, assetId, amount, sourceDomain, sourceSender, chainDomain, destinationRecepient));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        address signer = recoverSigner(ethSignedMessageHash, signature);
+        return signer;
+    }
+
+    function recoverSigner(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function splitSignature(
+        bytes memory sig
+    ) public pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            /*
+            First 32 bytes stores the length of the signature
+
+            add(sig, 32) = pointer of sig + 32
+            effectively, skips first 32 bytes of signature
+
+            mload(p) loads next 32 bytes starting at the memory address p into memory
+            */
+
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
+    }
+}
--- test/CCTPBridge.ts
+++ test/CCTPBridge.ts
@@ -1,10 +1,10 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { CCTPBridge, AD3, MockLssController} from "../typechain";
+import { ParamiCCTP, AD3, MockLssController} from "../typechain";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("CCTPBridge", function () {
-  let bridgeContract: CCTPBridge;
+  let bridgeContract: ParamiCCTP;
   let ad3Contract: AD3;
   let lssControllerContract: MockLssController;
   let deployer: SignerWithAddress;
@@ -18,15 +18,76 @@ describe("CCTPBridge", function () {
     const ad3Factory = await ethers.getContractFactory("AD3");
     ad3Contract = (await ad3Factory.deploy(10000, "AD3", "AD3", deployer.address, deployer.address, 10000, lssControllerContract.address)) as AD3;
 
-    const bridgeFactory = await ethers.getContractFactory("CCTPBridge");
-    bridgeContract = await bridgeFactory.deploy(ad3Contract.address);
+    const paramiCCTP = await ethers.getContractFactory("ParamiCCTP");
+    bridgeContract = await paramiCCTP.deploy(1);
   });
 
   it("Should Deposit Ad3", async function () {
+    const assetId = 10000;
+    await bridgeContract.registerAsset(assetId, ad3Contract.address);
     await ad3Contract.approve(bridgeContract.address, 100);
 
     const recepientBytes = ethers.utils.formatBytes32String("abdc");
+    const domain = 1;
 
-    await expect(bridgeContract.depositForBurn(100, 1, recepientBytes)).to.emit(bridgeContract, "Deposited").withArgs(deployer.address, 100, 1, recepientBytes);
+    await expect(bridgeContract.deposit(assetId, 100, 2, recepientBytes)).to.emit(bridgeContract, "Deposited").withArgs(1, assetId, 100, domain, deployer.address, 2, recepientBytes);
   });
+
+  it("Should withdraw", async function () {
+    const assetId = 10000;
+    const sourceDomain = 2;
+    const source = ethers.utils.formatBytes32String("abdc");
+    const nonce = 1;
+    const amount = 100;
+    const destDomain = 1;
+    const destAddr = deployer.address;
+
+    await bridgeContract.registerAsset(assetId, ad3Contract.address);
+    await ad3Contract.approve(bridgeContract.address, 100);
+    await bridgeContract.deposit(assetId, amount, sourceDomain, source);
+
+    const digest = generateMessageHash(nonce, sourceDomain, source, assetId, amount, destDomain, destAddr);
+
+    const signatureStringsig = await deployer.signMessage(ethers.utils.arrayify(digest));
+    console.log("", deployer.getAddress(), await bridgeContract.owner());
+
+    await expect(bridgeContract.withdraw(nonce, assetId, amount, sourceDomain, source, destAddr, signatureStringsig)).to.emit(bridgeContract, "Withdrawed").withArgs(nonce, assetId, amount, sourceDomain, source, destDomain, destAddr);
+  });
+
+  it("Should fail to withdraw if withdrawed before", async function () {
+    const assetId = 10000;
+    const sourceDomain = 2;
+    const source = ethers.utils.formatBytes32String("abdc");
+    const nonce = 1;
+    const amount = 100;
+    const destDomain = 1;
+    const destAddr = deployer.address;
+
+    await bridgeContract.registerAsset(assetId, ad3Contract.address);
+    await ad3Contract.approve(bridgeContract.address, 100);
+    await bridgeContract.deposit(assetId, amount, sourceDomain, source);
+
+    const digest = generateMessageHash(nonce, sourceDomain, source, assetId, amount, destDomain, destAddr);
+
+    const signatureStringsig = await deployer.signMessage(ethers.utils.arrayify(digest));
+    console.log("", deployer.getAddress(), await bridgeContract.owner());
+
+    await bridgeContract.withdraw(nonce, assetId, amount, sourceDomain, source, destAddr, signatureStringsig);
+    await expect(bridgeContract.withdraw(nonce, assetId, amount, sourceDomain, source, destAddr, signatureStringsig)).to.be.revertedWith("nonce already used");
+
+  });
+
 });
+
+function generateMessageHash(nonce: number,
+                     sourceDomain: number,
+                     sender: string,
+                     assetId: number,
+                     amount: number,
+                     destDomain: number,
+                     destinationRecepient: string,
+                     ): string {
+    const messageBytes = ethers.utils.solidityPack(['uint256', 'uint256', 'uint256', 'uint256', 'bytes', 'uint256', 'address'], [nonce, assetId, amount, sourceDomain, sender, destDomain, destinationRecepient]);
+    const messageDigest = ethers.utils.keccak256(messageBytes);
+    return messageDigest;
+}
