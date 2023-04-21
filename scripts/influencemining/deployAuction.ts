17299ffa41bdbab0ea46db5b04d0ea03e2f12aff
ironman_ch <coderfengyun@gmail.com>
2023-04-21 11:37:04 +0800

    MOD: deploy auction and governance

--- scripts/influencemining/deployAuction.ts
+++ scripts/influencemining/deployAuction.ts
@@ -6,10 +6,8 @@ async function deploy() {
   );
   const signer = await ethers.getSigners();
   console.log(signer[0].address)
-  const instance = await upgrades.deployProxy(factory, ["0x971dF9552669c3890AFE88Cb0a068C98a9A60B7C", "0xf6b2923717175185a626790FF78B6f37DAbb3565", "0x22CD32e81a43a8d99420f1fB6B47834039C58457"]);
+  const instance = await upgrades.deployProxy(factory, ["0x971dF9552669c3890AFE88Cb0a068C98a9A60B7C", "0xf6b2923717175185a626790FF78B6f37DAbb3565", "0x7c3826180814518C7d7765b1ECE11eDB708a7850"]);
   await instance.deployed();
-
-  await instance.transferOwnership("0x971dF9552669c3890AFE88Cb0a068C98a9A60B7C");
 
   console.log("auction proxy deployed to", instance.address);
 }
